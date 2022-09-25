c080d4d37e1e88787a3e7674a00805ffda0d306c
Sebastian Acosta <sacostamolina@outlook.com>
2022-09-24 22:27:14 -0700

    sorted algo

--- SortedList.cpp
+++ SortedList.cpp
@@ -34,3 +34,55 @@ int SortedList::binarySearch(int target, int low, int high)
 
 }
 
+int SortedList::partition(int arr[], int start, int end)
+{
+ 
+    int pivot = arr[start];
+ 
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+ 
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+ 
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i] <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+ 
+    return pivotIndex;
+}
+
+void SortedList::quickSort(int arr[], int start, int end)
+{
+ 
+    // base case
+    if (start >= end)
+        return;
+ 
+    // partitioning the array
+    int p = partition(arr, start, end);
+ 
+    // Sorting the left part
+    quickSort(arr, start, p - 1);
+ 
+    // Sorting the right part
+    quickSort(arr, p + 1, end);
+}
--- SortedList.hpp
+++ SortedList.hpp
@@ -6,13 +6,17 @@ using namespace std;
 
 //template <typename T>
 class SortedList: public List {
+
+private :
+	int partition(int arr[], int start, int end);
 public:
 	SortedList();
 	SortedList(int, int);
 	SortedList(int*, int);
 
 	int binarySearch( int target, int low, int high);
-
+	
+	void quickSort(int arr[], int start, int end);
 	
 };
 
